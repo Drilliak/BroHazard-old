abeea3f79a570427ea92bd3edd01161b538ff2a4
Drilliak <vinbat@hotmail.fr>
2017-03-12 12:38:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+BroHazard
+=========
+
+A Symfony project created on March 12, 2017, 11:29 am.
---
+++ src/TCS/PlatformBundle/TCSPlatformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TCS\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TCSPlatformBundle extends Bundle
+{
+}
